6df30f46173c0a4c328703b47a7689cb9ecda728
Boaofwoa <97913324+Boaofwoa@users.noreply.github.com>
2022-04-22 10:12:54 +0200

    Delete script directory

--- script/sticky-header.js
+++
@@ -1,8 +0,0 @@
-$(window).scroll(function() {
-    if ($(this).scrollTop() > 1){
-    $('.my-header').addClass("sticky");
-    }   
-    else{
-    $('.my-header').removeClass("sticky");
-    }
-    });
